14b27f460a3cd31f620c1e5135e952412fbc028b
Omenranr <Omen-retro@hotmail.fr>
2018-11-05 23:02:52 +0100

    Add files via upload

--- sommet.js
+++ sommet.js
@@ -6,12 +6,18 @@ function Sommet(x, y, rad) {
 	this.select = -1;
 	this.col = 255;
 	this.moved = false;
+	this.incan = function() {
+		if(mouseX < width && mouseY < height && mouseX > 0 && mouseY > 0) {
+			return true;
+		}
+		return false;
+	}
 	this.show = function() {
 		fill(this.col);
 		ellipse(this.x, this.y, 2*this.rad, 2*this.rad);
 	}
 	this.update = function(mouseX, mouseY) {
-		if(mouseX < width && mouseY < height) {
+		if(this.incan()) {
 			this.x = mouseX;
 		    this.y = mouseY;
 		}
